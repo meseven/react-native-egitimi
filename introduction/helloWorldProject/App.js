385ddebec6cb2463dcfd2d4da0337b8637c43d53
Mehmet Seven <mehmetseven0@gmail.com>
2019-01-19 17:24:37 +0300

    flex-direction

--- introduction/helloWorldProject/App.js
+++ introduction/helloWorldProject/App.js
@@ -7,11 +7,18 @@ export default class App extends Component<Props> {
     return (
       <View style={styles.container}>
 				<View style={styles.slideOne}></View>
-				<View style={styles.slideTwo}></View>
+				<View style={styles.slideTwo}>
+					<View style={[styles.box, styles.box1]}></View>
+					<View style={[styles.box, styles.box2]}></View>
+					<View style={[styles.box, styles.box3]}></View>
+				</View>
       </View>
     );
   }
 }
+
+// eğer flex-direction row verilirse birincil eksen yatay olur.
+// eğer flex-direction columnt verilirse birincil eksen dikey olur.
 
 const styles = StyleSheet.create({
 	container: {
@@ -20,10 +27,24 @@ const styles = StyleSheet.create({
   },
 	slideOne: {
 		backgroundColor: '#607D8B',
-		flex: 1 // 3:6
+		flex: 1
 	},
 	slideTwo: {
 		backgroundColor: '#FF5722',
-		flex: 1 // 3:6
+		flex: 2,
+		flexDirection: 'row'
+	},
+	box: {
+		width: 100,
+		height: 100
+	},
+	box1: {
+		backgroundColor:'#795548'
+	},
+	box2: {
+		backgroundColor: '#9E9E9E'
+	},
+	box3: {
+		backgroundColor: '#3F51B5'
 	}
 });
