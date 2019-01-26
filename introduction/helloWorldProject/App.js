bcb1d7408ec8741b52540080f43895acee7bb5c4
Mehmet Seven <mehmetseven0@gmail.com>
2019-01-26 17:22:14 +0300

    image uri

--- introduction/helloWorldProject/App.js
+++ introduction/helloWorldProject/App.js
@@ -5,9 +5,9 @@ export default class App extends Component {
 	render() {
 		return (
 			<View style={styles.container}>
-				<Image 
-					style={{width: '100%', height: 180}}
-					source={require('./src/assets/sweden.jpg')} />
+				<Image
+					style={{ width: '100%', height: 220 }}
+					source={{ uri: 'https://img2.10bestmedia.com/Images/Photos/239467/Looking-West-on-Beale-Street-SXN6oCYp2wAPsTW-KI2D-cr18q0ABlZBh-rgb-l_54_990x660_201404242159.jpg' }} />
 			</View>
 		);
 	}
