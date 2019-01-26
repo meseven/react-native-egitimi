052c87e07045c9f65b2fa7b7771d13f4c0145fec
Mehmet Seven <mehmetseven0@gmail.com>
2019-01-26 17:03:11 +0300

    pixel ratio image

--- introduction/helloWorldProject/App.js
+++ introduction/helloWorldProject/App.js
@@ -1,41 +1,13 @@
 import React, {Component} from 'react';
-import {StyleSheet, View, Button, Text} from 'react-native';
+import {StyleSheet, View, Image} from 'react-native';
 
 export default class App extends Component {
-	state = {
-		number: 0
-	};
-
-	onPressIncrease = () => {
-		this.setState({
-			number: ++this.state.number,
-		});
-	};
-
-	onPressDecrease = () => {
-		this.setState({
-			number: --this.state.number,
-		});
-	};
-
 	render() {
 		return (
 			<View style={styles.container}>
-				<View>
-					<Text style={styles.number}>{this.state.number}</Text>
-				</View>
-				<View style={styles.buttons}>
-					<Button
-						title='Azalt'
-						color='#000'
-						onPress={this.onPressDecrease}
-					/>
-					<Button
-						title='Arttır'
-						color='#000'
-						onPress={this.onPressIncrease}
-					/>
-				</View>
+				<Image 
+					style={{width: '100%', height: 180}}
+					source={require('./src/assets/sweden.jpg')} />
 			</View>
 		);
 	}
@@ -48,11 +20,5 @@ const styles = StyleSheet.create({
 		flexDirection: 'column',
 		justifyContent: 'center',
 		alignItems: 'center'
-	},
-	buttons: {
-		flexDirection: 'row'
-	},
-	number: {
-		fontSize: 44
 	}
 });
