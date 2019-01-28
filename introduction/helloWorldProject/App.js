41fd086c15cc41cab86440471cb7812b72227350
Mehmet Seven <mehmetseven0@gmail.com>
2019-01-28 18:33:29 +0300

    resize modes

--- introduction/helloWorldProject/App.js
+++ introduction/helloWorldProject/App.js
@@ -1,12 +1,21 @@
 import React, {Component} from 'react';
 import {StyleSheet, View, Image} from 'react-native';
 
+/*
+	- contain
+	- cover
+	- center
+	- repeat
+	- stretch
+ */
+
 export default class App extends Component {
 	render() {
 		return (
 			<View style={styles.container}>
 				<Image
-					style={{ width: '100%', height: 220 }}
+					resizeMode={'contain'}
+					style={{ width: '100%', height: 280, borderWidth: 2, borderColor:'red' }}
 					source={{ uri: 'https://img2.10bestmedia.com/Images/Photos/239467/Looking-West-on-Beale-Street-SXN6oCYp2wAPsTW-KI2D-cr18q0ABlZBh-rgb-l_54_990x660_201404242159.jpg' }} />
 			</View>
 		);
