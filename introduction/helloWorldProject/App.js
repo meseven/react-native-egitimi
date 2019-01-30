e521ef456b2cf673895e01701b1f354222fb233f
Mehmet Seven <mehmetseven0@gmail.com>
2019-01-30 16:23:03 +0300

    ScrollView

--- introduction/helloWorldProject/App.js
+++ introduction/helloWorldProject/App.js
@@ -1,22 +1,25 @@
 import React, {Component} from 'react';
-import {StyleSheet, View, Image} from 'react-native';
-
-/*
-	- contain
-	- cover
-	- center
-	- repeat
-	- stretch
- */
+import {StyleSheet, Text, View, Image, ScrollView} from 'react-native';
 
 export default class App extends Component {
 	render() {
 		return (
 			<View style={styles.container}>
-				<Image
-					resizeMode={'contain'}
-					style={{ width: '100%', height: 280, borderWidth: 2, borderColor:'red' }}
-					source={{ uri: 'https://img2.10bestmedia.com/Images/Photos/239467/Looking-West-on-Beale-Street-SXN6oCYp2wAPsTW-KI2D-cr18q0ABlZBh-rgb-l_54_990x660_201404242159.jpg' }} />
+				<ScrollView>
+					<Text style={styles.title}>1</Text>
+					<View>
+						<Text>hi!</Text>
+					</View>
+					<Text style={styles.title}>2</Text>
+					<Text style={styles.title}>3</Text>
+					<Text style={styles.title}>4</Text>
+					<Text style={styles.title}>5</Text>
+					<Text style={styles.title}>6</Text>
+					<Text style={styles.title}>7</Text>
+					<Text style={styles.title}>8</Text>
+					<Text style={styles.title}>9</Text>
+					<Text style={styles.title}>10</Text>
+				</ScrollView>
 			</View>
 		);
 	}
@@ -26,8 +29,12 @@ const styles = StyleSheet.create({
 	container: {
 		backgroundColor: '#FFEB3B',
 		flex: 1,
-		flexDirection: 'column',
-		justifyContent: 'center',
-		alignItems: 'center'
+		flexDirection: 'row',
+	},
+	title: {
+		backgroundColor: 'orange',
+		marginVertical: 30,
+		textAlign: 'center',
+		paddingVertical: 60
 	}
 });
