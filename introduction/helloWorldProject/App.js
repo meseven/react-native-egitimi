76567d7836f88ea8bfc1204a19ead7898a1ffc98
Mehmet Seven <mehmetseven0@gmail.com>
2019-01-31 17:44:17 +0300

    FlatList2

--- introduction/helloWorldProject/App.js
+++ introduction/helloWorldProject/App.js
@@ -1,14 +1,20 @@
 import React, {Component} from 'react';
-import { StyleSheet, View, Text, SafeAreaView, FlatList } from 'react-native';
+import { StyleSheet, View, Text, SafeAreaView, FlatList, Image, TouchableOpacity } from 'react-native';
 
 import data from './data';
 
 export default class App extends Component {
 	renderContactsItem = ({ item, index }) => {
 		return(
-			<View>
-				<Text>{item.name}</Text>
-			</View>
+			<TouchableOpacity style={styles.itemContainer}>
+				<Image
+					style={styles.avatar}
+					source={{ uri: item.picture }} />
+				<View style={styles.textContainer}>
+					<Text style={styles.name}>{item.name}</Text>
+					<Text>{item.company}</Text>
+				</View>
+			</TouchableOpacity>
 		)
 	};
 
@@ -29,5 +35,24 @@ export default class App extends Component {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1
+	},
+	itemContainer: {
+		flex: 1,
+		flexDirection: 'row',
+		paddingVertical: 10,
+		borderBottomWidth: 1,
+		borderBottomColor: '#eee'
+	},
+	avatar: {
+		width: 50,
+		height: 50,
+		borderRadius: 25,
+		marginHorizontal: 10
+	},
+	textContainer: {
+		justifyContent: 'space-around'
+	},
+	name: {
+		fontSize: 16
 	}
 });
--- introduction/helloWorldProject/data.js
+++ introduction/helloWorldProject/data.js
@@ -1,289 +1,289 @@
 export default [
 	{
 		"_id": "5c52de4c0f4ed2ae1bff8739",
-		"picture": "http://placehold.it/32x32",
+		"picture": "https://randomuser.me/api/portraits/men/1.jpg",
 		"name": "Webster Burgess",
 		"company": "EQUITOX"
 	},
 	{
 		"_id": "5c52de4cac35a17d8c0993c1",
-		"picture": "http://placehold.it/32x32",
+		"picture": "https://randomuser.me/api/portraits/men/2.jpg",
 		"name": "Medina Glenn",
 		"company": "CYCLONICA"
 	},
 	{
 		"_id": "5c52de4c54f2cb9b2eaca28e",
-		"picture": "http://placehold.it/32x32",
+		"picture": "https://randomuser.me/api/portraits/men/3.jpg",
 		"name": "Ernestine Reyes",
 		"company": "YURTURE"
 	},
 	{
 		"_id": "5c52de4c27a7eaf7b64d44ca",
-		"picture": "http://placehold.it/32x32",
+		"picture": "https://randomuser.me/api/portraits/men/4.jpg",
 		"name": "Patrica Hess",
 		"company": "MENBRAIN"
 	},
 	{
 		"_id": "5c52de4c470775a1e4041379",
-		"picture": "http://placehold.it/32x32",
+		"picture": "https://randomuser.me/api/portraits/men/5.jpg",
 		"name": "Lawrence Sherman",
 		"company": "SCENTRIC"
 	},
 	{
 		"_id": "5c52de4c491139efcc4d75cf",
-		"picture": "http://placehold.it/32x32",
+		"picture": "https://randomuser.me/api/portraits/men/6.jpg",
 		"name": "Tessa Erickson",
 		"company": "AUSTECH"
 	},
 	{
 		"_id": "5c52de4cb83143009752997c",
-		"picture": "http://placehold.it/32x32",
+		"picture": "https://randomuser.me/api/portraits/men/7.jpg",
 		"name": "Crystal Doyle",
 		"company": "ZILCH"
 	},
 	{
 		"_id": "5c52de4c4e4ae4ea190a4743",
-		"picture": "http://placehold.it/32x32",
+		"picture": "https://randomuser.me/api/portraits/men/8.jpg",
 		"name": "Reyna Bradley",
 		"company": "CUIZINE"
 	},
 	{
 		"_id": "5c52de4c916c0ec176cc25ab",
-		"picture": "http://placehold.it/32x32",
+		"picture": "https://randomuser.me/api/portraits/men/9.jpg",
 		"name": "Estelle Gallagher",
 		"company": "INTERODEO"
 	},
 	{
 		"_id": "5c52de4cf9da515d14735190",
-		"picture": "http://placehold.it/32x32",
+		"picture": "https://randomuser.me/api/portraits/men/10.jpg",
 		"name": "Hendrix Rojas",
 		"company": "MAKINGWAY"
 	},
 	{
 		"_id": "5c52de4c42ef39a57f1bd1ee",
-		"picture": "http://placehold.it/32x32",
+		"picture": "https://randomuser.me/api/portraits/men/11.jpg",
 		"name": "Kimberly Nieves",
 		"company": "XERONK"
 	},
 	{
 		"_id": "5c52de4cfdf070b1c0f5ed34",
-		"picture": "http://placehold.it/32x32",
+		"picture": "https://randomuser.me/api/portraits/men/12.jpg",
 		"name": "Felecia Mcleod",
 		"company": "LIMAGE"
 	},
 	{
 		"_id": "5c52de4c157415a10ad2f39e",
-		"picture": "http://placehold.it/32x32",
+		"picture": "https://randomuser.me/api/portraits/men/13.jpg",
 		"name": "Enid Blankenship",
 		"company": "ECLIPTO"
 	},
 	{
 		"_id": "5c52de4c94f0146ee1da0a39",
-		"picture": "http://placehold.it/32x32",
+		"picture": "https://randomuser.me/api/portraits/men/14.jpg",
 		"name": "Rhodes Blanchard",
 		"company": "HAIRPORT"
 	},
 	{
 		"_id": "5c52de4ccae10cfc264ea5e0",
-		"picture": "http://placehold.it/32x32",
+		"picture": "https://randomuser.me/api/portraits/men/15.jpg",
 		"name": "Craig Woodard",
 		"company": "KAGE"
 	},
 	{
 		"_id": "5c52de4c47d12f28075e233b",
-		"picture": "http://placehold.it/32x32",
+		"picture": "https://randomuser.me/api/portraits/men/16.jpg",
 		"name": "Mathews Clements",
 		"company": "HALAP"
 	},
 	{
 		"_id": "5c52de4c277356ce5898add2",
-		"picture": "http://placehold.it/32x32",
+		"picture": "https://randomuser.me/api/portraits/men/17.jpg",
 		"name": "Patti Burke",
 		"company": "MAGNEATO"
 	},
 	{
 		"_id": "5c52de4c3da0e66a28848c49",
-		"picture": "http://placehold.it/32x32",
+		"picture": "https://randomuser.me/api/portraits/men/18.jpg",
 		"name": "Catalina Thompson",
 		"company": "AFFLUEX"
 	},
 	{
 		"_id": "5c52de4cdc5398d1d0e2e613",
-		"picture": "http://placehold.it/32x32",
+		"picture": "https://randomuser.me/api/portraits/men/19.jpg",
 		"name": "Soto Dale",
 		"company": "POSHOME"
 	},
 	{
 		"_id": "5c52de4c281e927761f597d8",
-		"picture": "http://placehold.it/32x32",
+		"picture": "https://randomuser.me/api/portraits/men/20.jpg",
 		"name": "Downs Williams",
 		"company": "HANDSHAKE"
 	},
 	{
 		"_id": "5c52de4ca2eba72ff8b19bbf",
-		"picture": "http://placehold.it/32x32",
+		"picture": "https://randomuser.me/api/portraits/men/21.jpg",
 		"name": "Townsend Mckee",
 		"company": "UNIWORLD"
 	},
 	{
 		"_id": "5c52de4c8abcc5cfdf8a62f7",
-		"picture": "http://placehold.it/32x32",
+		"picture": "https://randomuser.me/api/portraits/men/22.jpg",
 		"name": "Washington Serrano",
 		"company": "ZUVY"
 	},
 	{
 		"_id": "5c52de4cd45a1e52209e3d35",
-		"picture": "http://placehold.it/32x32",
+		"picture": "https://randomuser.me/api/portraits/men/23.jpg",
 		"name": "Georgia Dixon",
 		"company": "PEARLESSA"
 	},
 	{
 		"_id": "5c52de4ca30e999fd297f483",
-		"picture": "http://placehold.it/32x32",
+		"picture": "https://randomuser.me/api/portraits/men/24.jpg",
 		"name": "Mona Parker",
 		"company": "INSURITY"
 	},
 	{
 		"_id": "5c52de4c135e3122a92916e4",
-		"picture": "http://placehold.it/32x32",
+		"picture": "https://randomuser.me/api/portraits/men/25.jpg",
 		"name": "Slater Brewer",
 		"company": "VORTEXACO"
 	},
 	{
 		"_id": "5c52de4c09d3e98855c67981",
-		"picture": "http://placehold.it/32x32",
+		"picture": "https://randomuser.me/api/portraits/men/26.jpg",
 		"name": "Reilly Barron",
 		"company": "ANDERSHUN"
 	},
 	{
 		"_id": "5c52de4c43f2985efaca8ca8",
-		"picture": "http://placehold.it/32x32",
+		"picture": "https://randomuser.me/api/portraits/men/27.jpg",
 		"name": "Doreen Cummings",
 		"company": "CODAX"
 	},
 	{
 		"_id": "5c52de4c188e45c9d2bcb429",
-		"picture": "http://placehold.it/32x32",
+		"picture": "https://randomuser.me/api/portraits/men/28.jpg",
 		"name": "Hood Flores",
 		"company": "ORGANICA"
 	},
 	{
 		"_id": "5c52de4c5ede5c3037c54a39",
-		"picture": "http://placehold.it/32x32",
+		"picture": "https://randomuser.me/api/portraits/men/29.jpg",
 		"name": "Pansy Tucker",
 		"company": "SHOPABOUT"
 	},
 	{
 		"_id": "5c52de4c0a312078fdf62dc9",
-		"picture": "http://placehold.it/32x32",
+		"picture": "https://randomuser.me/api/portraits/men/30.jpg",
 		"name": "Joanne Phillips",
 		"company": "CIRCUM"
 	},
 	{
 		"_id": "5c52de4c28d9507099a79f85",
-		"picture": "http://placehold.it/32x32",
+		"picture": "https://randomuser.me/api/portraits/men/31.jpg",
 		"name": "Duncan Roberson",
 		"company": "EPLOSION"
 	},
 	{
 		"_id": "5c52de4c5717df3361de5d9e",
-		"picture": "http://placehold.it/32x32",
+		"picture": "https://randomuser.me/api/portraits/men/32.jpg",
 		"name": "Kemp Ward",
 		"company": "XINWARE"
 	},
 	{
 		"_id": "5c52de4cbf590fd683fde1f3",
-		"picture": "http://placehold.it/32x32",
+		"picture": "https://randomuser.me/api/portraits/men/33.jpg",
 		"name": "Scott Peterson",
 		"company": "GEOFORMA"
 	},
 	{
 		"_id": "5c52de4c853220c9458f9f34",
-		"picture": "http://placehold.it/32x32",
+		"picture": "https://randomuser.me/api/portraits/men/34.jpg",
 		"name": "Adriana Carter",
 		"company": "COMTRACT"
 	},
 	{
 		"_id": "5c52de4c2b503a2e56f755aa",
-		"picture": "http://placehold.it/32x32",
+		"picture": "https://randomuser.me/api/portraits/men/35.jpg",
 		"name": "Fannie Travis",
 		"company": "TELEPARK"
 	},
 	{
 		"_id": "5c52de4c8fca3d8a5f821c1d",
-		"picture": "http://placehold.it/32x32",
+		"picture": "https://randomuser.me/api/portraits/men/36.jpg",
 		"name": "Josefa Atkinson",
 		"company": "VIAGRAND"
 	},
 	{
 		"_id": "5c52de4c620f75877df84b90",
-		"picture": "http://placehold.it/32x32",
+		"picture": "https://randomuser.me/api/portraits/men/37.jpg",
 		"name": "Rosalind Ford",
 		"company": "EURON"
 	},
 	{
 		"_id": "5c52de4c953b4bec45f8d0db",
-		"picture": "http://placehold.it/32x32",
+		"picture": "https://randomuser.me/api/portraits/men/38.jpg",
 		"name": "Melton Valdez",
 		"company": "HOMELUX"
 	},
 	{
 		"_id": "5c52de4cc731db814480c166",
-		"picture": "http://placehold.it/32x32",
+		"picture": "https://randomuser.me/api/portraits/men/39.jpg",
 		"name": "Whitaker Weeks",
 		"company": "TELPOD"
 	},
 	{
 		"_id": "5c52de4cc90d14bc6c4ffe3b",
-		"picture": "http://placehold.it/32x32",
+		"picture": "https://randomuser.me/api/portraits/men/40.jpg",
 		"name": "Della Brady",
 		"company": "SYNTAC"
 	},
 	{
 		"_id": "5c52de4cb60cf682a8db9f87",
-		"picture": "http://placehold.it/32x32",
+		"picture": "https://randomuser.me/api/portraits/men/41.jpg",
 		"name": "Lynch Winters",
 		"company": "VICON"
 	},
 	{
 		"_id": "5c52de4cf7066656d5d162a6",
-		"picture": "http://placehold.it/32x32",
+		"picture": "https://randomuser.me/api/portraits/men/42.jpg",
 		"name": "Margaret Day",
 		"company": "SIGNITY"
 	},
 	{
 		"_id": "5c52de4ca51bb600c6579963",
-		"picture": "http://placehold.it/32x32",
+		"picture": "https://randomuser.me/api/portraits/men/43.jpg",
 		"name": "Fischer Bush",
 		"company": "DUOFLEX"
 	},
 	{
 		"_id": "5c52de4ca0ae72e0a293a294",
-		"picture": "http://placehold.it/32x32",
+		"picture": "https://randomuser.me/api/portraits/men/44.jpg",
 		"name": "Aileen Greer",
 		"company": "EYERIS"
 	},
 	{
 		"_id": "5c52de4c7f102459b57142b3",
-		"picture": "http://placehold.it/32x32",
+		"picture": "https://randomuser.me/api/portraits/men/45.jpg",
 		"name": "Yesenia Walsh",
 		"company": "ECLIPSENT"
 	},
 	{
 		"_id": "5c52de4c970f2a6247995343",
-		"picture": "http://placehold.it/32x32",
+		"picture": "https://randomuser.me/api/portraits/men/46.jpg",
 		"name": "Summers Herman",
 		"company": "MAGNINA"
 	},
 	{
 		"_id": "5c52de4c25048abc1d13e86d",
-		"picture": "http://placehold.it/32x32",
+		"picture": "https://randomuser.me/api/portraits/men/47.jpg",
 		"name": "Mcneil Cooke",
 		"company": "COMTREK"
 	},
 	{
 		"_id": "5c52de4c4211b38a38eaac2b",
-		"picture": "http://placehold.it/32x32",
+		"picture": "https://randomuser.me/api/portraits/men/48.jpg",
 		"name": "Newman Franklin",
 		"company": "FORTEAN"
 	}
