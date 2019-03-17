ea6692fce0ce833caabd50e6f23e2ed8e8483df0
Mehmet Seven <mehmetseven0@gmail.com>
2019-03-17 16:32:31 +0300

    axios

--- introduction/helloWorldProject/App.js
+++ introduction/helloWorldProject/App.js
@@ -1,13 +1,42 @@
 import React, {Component} from 'react';
-import { StyleSheet,SafeAreaView } from 'react-native';
+import { StyleSheet,SafeAreaView,View, Text } from 'react-native';
 
 import FlatListExample from './src/components/FlatListExample';
 
+import axios from 'axios';
+
 export default class App extends Component {
+	state = {
+		name: '',
+		surname: '',
+		loading: true
+	};
+
+	componentDidMount() {
+		axios
+			.get('https://randomuser.me/api/')
+			.then(user => user.data.results[0])
+			.then(user => {
+				this.setState({
+					name: user.name.first,
+					surname: user.name.last,
+					loading: false
+				});
+			})
+	}
+
 	render() {
+		const {name,surname,loading} = this.state;
 		return (
 			<SafeAreaView style={styles.container}>
-				<FlatListExample />
+				{/*<FlatListExample />*/}
+				<View>
+					{
+						loading ? <Text>Loading...</Text> :
+							<Text>{name} {surname}</Text>
+					}
+
+				</View>
 			</SafeAreaView>
 		);
 	}
@@ -15,6 +44,8 @@ export default class App extends Component {
 
 const styles = StyleSheet.create({
 	container: {
-		flex: 1
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center'
 	}
 });
