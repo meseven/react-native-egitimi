c4bea20be3d15e298482b34a350cec5dd1d87935
Mehmet Seven <mehmetseven0@gmail.com>
2019-03-18 19:03:53 +0300

    navigate method

--- react-navigation/NavigationApp/src/screens/DetailScreen.js
+++ react-navigation/NavigationApp/src/screens/DetailScreen.js
@@ -1,12 +1,16 @@
-import {Component} from "react";
-import {StyleSheet, Text, View} from "react-native";
-import React from "react";
+import React, {Component} from "react";
+import {Button, StyleSheet, Text, View} from "react-native";
 
 export default class DetailScreen extends Component {
 	render() {
+		const { navigate } = this.props.navigation;
 		return (
 			<View style={styles.container}>
 				<Text>Detail</Text>
+				<Button
+					title="go to home page"
+					onPress={() => navigate('Home')}
+				/>
 			</View>
 		);
 	}
--- react-navigation/NavigationApp/src/screens/HomeScreen.js
+++ react-navigation/NavigationApp/src/screens/HomeScreen.js
@@ -1,12 +1,16 @@
-import {Component} from "react";
-import {StyleSheet, Text, View} from "react-native";
-import React from "react";
+import React,{Component} from "react";
+import {StyleSheet, Text, View, Button} from "react-native";
 
 export default class HomeScreen extends Component {
 	render() {
+		const { navigate } = this.props.navigation;
 		return (
 			<View style={styles.container}>
 				<Text>Home</Text>
+				<Button
+					title="go to detail page"
+					onPress={() => navigate('Detail')}
+				/>
 			</View>
 		);
 	}
