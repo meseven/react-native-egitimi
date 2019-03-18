269f403de598bffa4bf9d4a7b4648dc44949bc8f
Mehmet Seven <mehmetseven0@gmail.com>
2019-03-18 19:43:41 +0300

    goback method

--- react-navigation/NavigationApp/src/screens/DetailScreen.js
+++ react-navigation/NavigationApp/src/screens/DetailScreen.js
@@ -3,13 +3,18 @@ import {Button, StyleSheet, Text, View} from "react-native";
 
 export default class DetailScreen extends Component {
 	render() {
-		const { navigate, push } = this.props.navigation;
+		const { navigate, push, goBack } = this.props.navigation;
 		return (
 			<View style={styles.container}>
 				<Text>Detail</Text>
 				<Button
 					title="go to home page"
-					onPress={() => push('Home')}
+					onPress={() => push('Detail')}
+				/>
+
+				<Button
+					title="go back"
+					onPress={() => goBack()}
 				/>
 			</View>
 		);
