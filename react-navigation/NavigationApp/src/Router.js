bb85c8bd2976592417137aff1afd73fa4224ad55
Mehmet Seven <mehmetseven0@gmail.com>
2019-03-20 21:47:13 +0300

    drawer navigator customizations

--- react-navigation/NavigationApp/src/Router.js
+++ react-navigation/NavigationApp/src/Router.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { createStackNavigator, createAppContainer, createDrawerNavigator } from 'react-navigation';
 
 import HomeScreen from './screens/HomeScreen';
@@ -6,6 +7,8 @@ import DetailScreen from './screens/DetailScreen';
 import ContactScreen from './screens/ContactScreen';
 
 // import AboutModal from './screens/AboutModal';
+
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const HomeStack = createStackNavigator({
 	Home: {
@@ -24,10 +27,46 @@ const ContactStack = createStackNavigator({
 
 const Drawer = createDrawerNavigator({
 	Home: {
-		screen: HomeStack
+		screen: HomeStack,
+		navigationOptions: {
+			drawerLabel: 'Home',
+			drawerIcon: ({ tintColor }) => (
+				<Icon
+					name="ios-home"
+					size={22}
+					color={tintColor}
+				/>
+			)
+		}
 	},
 	Contact: {
-		screen: ContactStack
+		screen: ContactStack,
+		navigationOptions: {
+			drawerLockMode: 'locked-closed',
+			drawerLabel: 'Contact',
+			drawerIcon: ({ tintColor }) => (
+				<Icon
+					name="ios-contact"
+					size={22}
+					color={tintColor}
+				/>
+			),
+		}
+	}
+}, {
+	drawerPosition: 'right',
+	drawerWidth: 160,
+	contentOptions: {
+		activeTintColor: '#e91e63',
+		itemsContainerStyle: {
+			opacity: 1
+		},
+		iconContainerStyle: {
+			opacity: 1
+		},
+		itemStyle: {
+			flexDirection: 'row-reverse'
+		}
 	}
 });
 
