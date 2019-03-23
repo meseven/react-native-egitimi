981569ea57faebc57da259032d702deb9c76cdb2
Mehmet Seven <mehmetseven0@gmail.com>
2019-03-24 00:07:12 +0300

    react native image picker installations

--- image-picker/ImagePickerExample/App.js
+++ image-picker/ImagePickerExample/App.js
@@ -1,29 +1,56 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Image, View, Button} from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import ImagePicker from 'react-native-image-picker';
 
-type Props = {};
+// More info on all the options is below in the API Reference... just some common use cases shown here
+const options = {
+	title: 'Select Avatar',
+	customButtons: [{ name: 'fb', title: 'Choose Photo from Facebook' }],
+	storageOptions: {
+		skipBackup: true,
+		path: 'images',
+	},
+};
+
 export default class App extends Component<Props> {
+
+	state = {
+		avatarSource: null
+	};
+
+  onSelectPicture = () => {
+		ImagePicker.showImagePicker(options, (response) => {
+			console.log('Response = ', response);
+
+			if (response.didCancel) {
+				console.log('User cancelled image picker');
+			} else if (response.error) {
+				console.log('ImagePicker Error: ', response.error);
+			} else if (response.customButton) {
+				console.log('User tapped custom button: ', response.customButton);
+			} else {
+				const source = { uri: response.uri };
+
+				// You can also display the image using data:
+				// const source = { uri: 'data:image/jpeg;base64,' + response.data };
+
+				this.setState({
+					avatarSource: source,
+				});
+			}
+		});
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+				<Image source={this.state.avatarSource} style={{ width: 200, height: 200 }} />
+
+				<Button
+          title={"Select Picture"}
+          onPress={this.onSelectPicture}
+        />
       </View>
     );
   }
@@ -35,15 +62,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  }
 });
