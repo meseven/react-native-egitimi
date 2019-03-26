52eaa011ed44c0ccdde6e373419d79a300f2937c
Mehmet Seven <mehmetseven0@gmail.com>
2019-03-26 17:27:04 +0300

    test

--- camera/CameraExampleProject/App.js
+++ camera/CameraExampleProject/App.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
 import CameraView from './components/CameraView';
 
 export default class App extends Component {
