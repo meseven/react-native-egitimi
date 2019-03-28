2b571f50fe85f54bd19d30ee24c3145ab53a521a
Mehmet Seven <mehmetseven0@gmail.com>
2019-03-28 18:34:29 +0300

    absolute positions

--- animations/AnimationExamples/components/AbsolutePosition.js
+++ animations/AnimationExamples/components/AbsolutePosition.js
@@ -2,11 +2,30 @@ import React, { Component } from 'react';
 import {Animated, StyleSheet, Text, TouchableWithoutFeedback, View} from 'react-native';
 
 export default class AbsolutePosition extends Component {
+	state = {
+		animation: new Animated.Value(0)
+	};
+
+	startAnimation = () => {
+		Animated
+			.timing(this.state.animation, {
+				duration: 500,
+				toValue: 30
+			})
+			.start()
+	}
+
 	render() {
+		const animatedStyles = {
+			left: this.state.animation,
+			right: this.state.animation,
+			bottom: this.state.animation
+		};
+
 		return (
 			<View style={styles.container}>
 				<TouchableWithoutFeedback onPress={this.startAnimation}>
-					<Animated.View style={[styles.myBox]}>
+					<Animated.View style={[styles.myBox, animatedStyles]}>
 						<Text>React Native</Text>
 					</Animated.View>
 				</TouchableWithoutFeedback>
@@ -23,10 +42,13 @@ const styles = StyleSheet.create({
 		backgroundColor: '#F5FCFF',
 	},
 	myBox: {
-		width: 200,
 		height: 200,
 		backgroundColor: '#FFC107',
 		justifyContent:'center',
-		alignItems:'center'
+		alignItems:'center',
+		position: 'absolute',
+		bottom: 0,
+		left: 0,
+		right:0
 	}
 });
