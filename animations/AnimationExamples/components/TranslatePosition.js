f6e250597baafd4d08511daf6067670e19f39a4c
Mehmet Seven <mehmetseven0@gmail.com>
2019-03-28 16:58:20 +0300

    translate position

--- animations/AnimationExamples/components/TranslatePosition.js
+++ animations/AnimationExamples/components/TranslatePosition.js
@@ -2,11 +2,32 @@ import React, { Component } from 'react';
 import {Animated, StyleSheet, Text, TouchableWithoutFeedback, View} from 'react-native';
 
 export default class Timing extends Component {
+	state = {
+		animation: new Animated.Value(0)
+	};
+
+	startAnimation = () => {
+		Animated
+			.timing(this.state.animation, {
+				toValue: 200,
+				duration: 300
+			})
+			.start()
+	};
+
 	render() {
+		const animatedStyles = {
+			transform: [
+				{
+					translateX: this.state.animation
+				}
+			]
+		};
+
 		return (
 			<View style={styles.container}>
 				<TouchableWithoutFeedback onPress={this.startAnimation}>
-					<Animated.View style={[styles.myBox]}>
+					<Animated.View style={[styles.myBox, animatedStyles]}>
 						<Text>React Native</Text>
 					</Animated.View>
 				</TouchableWithoutFeedback>
