331646e916ae892bccfad7042c684485ccd0cd48
Mehmet Seven <mehmetseven0@gmail.com>
2019-03-28 18:17:47 +0300

    multi animation field

--- animations/AnimationExamples/components/MultiField.js
+++ animations/AnimationExamples/components/MultiField.js
@@ -2,11 +2,38 @@ import React, { Component } from 'react';
 import {Animated, StyleSheet, Text, TouchableWithoutFeedback, View} from 'react-native';
 
 export default class MultiField extends Component {
+	state = {
+		animationWidth: new Animated.Value(200),
+		animationHeight: new Animated.Value(200)
+	};
+
+	startAnimation = () => {
+		Animated
+			.timing(this.state.animationWidth, {
+				duration: 300,
+				toValue: 350
+			})
+			.start()
+
+
+		Animated
+			.timing(this.state.animationHeight, {
+				duration:500,
+				toValue: 350
+			})
+			.start()
+	};
+
 	render() {
+		const animatedStyles= {
+			width: this.state.animationWidth,
+			height: this.state.animationHeight
+		};
+
 		return (
 			<View style={styles.container}>
 				<TouchableWithoutFeedback onPress={this.startAnimation}>
-					<Animated.View style={[styles.myBox]}>
+					<Animated.View style={[styles.myBox, animatedStyles]}>
 						<Text>React Native</Text>
 					</Animated.View>
 				</TouchableWithoutFeedback>
@@ -23,8 +50,8 @@ const styles = StyleSheet.create({
 		backgroundColor: '#F5FCFF',
 	},
 	myBox: {
-		width: 200,
-		height: 200,
+		// width: 200,
+		// height: 200,
 		backgroundColor: '#FFC107',
 		justifyContent:'center',
 		alignItems:'center'
