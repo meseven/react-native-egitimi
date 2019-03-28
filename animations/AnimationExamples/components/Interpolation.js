1c0af2ff128bb3db980e9263a63bc9d6938a14d2
Mehmet Seven <mehmetseven0@gmail.com>
2019-03-28 21:15:06 +0300

    interpolate

--- animations/AnimationExamples/components/Interpolation.js
+++ animations/AnimationExamples/components/Interpolation.js
@@ -2,12 +2,43 @@ import React, { Component } from 'react';
 import {Animated, StyleSheet, Text, TouchableWithoutFeedback, View} from 'react-native';
 
 export default class Interpolation extends Component {
+	state = {
+		animation: new Animated.Value(0)
+	};
+
+	startAnimation = () => {
+		Animated
+			.timing(this.state.animation, {
+				duration: 500,
+				toValue: 1
+			})
+			.start()
+	};
+
 	render() {
+		const boxInterpolate = this.state.animation.interpolate({
+			inputRange: [0, 1],
+			outputRange: ['red', 'black']
+		});
+
+		const textInterpolate = this.state.animation.interpolate({
+			inputRange: [0, 1],
+			outputRange: ['black', 'white']
+		});
+
+		const boxAnimationStyles = {
+			backgroundColor: boxInterpolate
+		};
+
+		const textAnimationStyles = {
+			color: textInterpolate
+		};
+
 		return (
 			<View style={styles.container}>
 				<TouchableWithoutFeedback onPress={this.startAnimation}>
-					<Animated.View style={[styles.myBox]}>
-						<Text>React Native</Text>
+					<Animated.View style={[styles.myBox, boxAnimationStyles]}>
+						<Animated.Text style={textAnimationStyles}>React Native</Animated.Text>
 					</Animated.View>
 				</TouchableWithoutFeedback>
 			</View>
