649ea38494795a0372ac8f5224199ab651229bb7
Mehmet Seven <mehmetseven0@gmail.com>
2019-03-28 21:38:51 +0300

    rotate interpolation

--- animations/AnimationExamples/components/InterpolationRotate.js
+++ animations/AnimationExamples/components/InterpolationRotate.js
@@ -2,11 +2,35 @@ import React, { Component } from 'react';
 import {Animated, StyleSheet, Text, TouchableWithoutFeedback, View} from 'react-native';
 
 export default class InterpolationRotate extends Component {
+	state = {
+		animation: new Animated.Value(0)
+	};
+
+	startAnimation = () => {
+		Animated
+			.timing(this.state.animation, {
+				duration: 1000,
+				toValue: 360
+			})
+			.start()
+	};
+
 	render() {
+		const interpolation = this.state.animation.interpolate({
+			inputRange: [0, 360],
+			outputRange: ['0deg', '1080deg']
+		});
+
+		const animatedStyles = {
+			transform: [{
+				rotate: interpolation
+			}]
+		};
+
 		return (
 			<View style={styles.container}>
 				<TouchableWithoutFeedback onPress={this.startAnimation}>
-					<Animated.View style={[styles.myBox]}>
+					<Animated.View style={[styles.myBox, animatedStyles]}>
 						<Text>React Native</Text>
 					</Animated.View>
 				</TouchableWithoutFeedback>
