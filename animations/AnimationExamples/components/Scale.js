2b50466dcc59e02bdea49d1212c7c45f05f9765f
Mehmet Seven <mehmetseven0@gmail.com>
2019-03-28 17:29:39 +0300

    scale transform

--- animations/AnimationExamples/components/Scale.js
+++ animations/AnimationExamples/components/Scale.js
@@ -2,11 +2,32 @@ import React, { Component } from 'react';
 import {Animated, StyleSheet, Text, TouchableWithoutFeedback, View} from 'react-native';
 
 export default class Timing extends Component {
+	state = {
+		animation: new Animated.Value(1)
+	};
+
+	startAnimation = () => {
+		Animated
+			.spring(this.state.animation, {
+				duration: 500,
+				toValue: -1
+			})
+			.start();
+	};
+
 	render() {
+		const animatedStyles = {
+			transform: [
+				{
+					scaleX: this.state.animation
+				}
+			]
+		};
+
 		return (
 			<View style={styles.container}>
 				<TouchableWithoutFeedback onPress={this.startAnimation}>
-					<Animated.View style={[styles.myBox]}>
+					<Animated.View style={[styles.myBox, animatedStyles]}>
 						<Text>React Native</Text>
 					</Animated.View>
 				</TouchableWithoutFeedback>
