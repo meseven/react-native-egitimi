9e6059d007aa000fab56030456a5ff81f7a4d32b
Mehmet Seven <mehmetseven0@gmail.com>
2019-03-28 19:12:39 +0300

    interpolation

--- animations/AnimationExamples/App.js
+++ animations/AnimationExamples/App.js
@@ -1,15 +1,16 @@
 import React, {Component} from 'react';
 
-import Timing from './components/Timing';
-import TranslatePosition from './components/TranslatePosition';
-import Scale from './components/Scale';
-import MultiField from './components/MultiField';
-import AbsolutePosition from './components/AbsolutePosition';
+// import Timing from './components/Timing';
+// import TranslatePosition from './components/TranslatePosition';
+// import Scale from './components/Scale';
+// import MultiField from './components/MultiField';
+// import AbsolutePosition from './components/AbsolutePosition';
+import Interpolation from './components/Interpolation';
 
 export default class App extends Component {
 	render() {
 		return (
-			<AbsolutePosition />
+			<Interpolation />
 		);
 	}
 }
