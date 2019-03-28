e86f42764519d8baf52d617fe7f95ee03f8f16c6
Mehmet Seven <mehmetseven0@gmail.com>
2019-03-28 22:56:13 +0300

    spring

--- animations/AnimationExamples/components/Spring2.js
+++ animations/AnimationExamples/components/Spring2.js
@@ -2,11 +2,30 @@ import React, { Component } from 'react';
 import {Animated, StyleSheet, Text, TouchableWithoutFeedback, View} from 'react-native';
 
 export default class Spring extends Component {
+	state = {
+		animation: new Animated.Value(0)
+	};
+
+	startAnimation = () => {
+		Animated
+			.spring(this.state.animation, {
+				duration: 300,
+				toValue: 30
+			})
+			.start()
+	};
+
 	render() {
+		const animatedStyles = {
+			bottom: this.state.animation,
+			left: this.state.animation,
+			right: this.state.animation,
+		};
+
 		return (
 			<View style={styles.container}>
 				<TouchableWithoutFeedback onPress={this.startAnimation}>
-					<Animated.View style={[styles.myBox]}>
+					<Animated.View style={[styles.myBox, animatedStyles]}>
 						<Text>React Native</Text>
 					</Animated.View>
 				</TouchableWithoutFeedback>
@@ -23,7 +42,10 @@ const styles = StyleSheet.create({
 		backgroundColor: '#F5FCFF',
 	},
 	myBox: {
-		width: 100,
+		position:'absolute',
+		bottom:0,
+		left: 0,
+		right:0,
 		height: 100,
 		backgroundColor: '#FFC107',
 		justifyContent:'center',
