74e3a0bee559c3eea07b40860d40ac12cc79b5b3
Mehmet Seven <mehmetseven0@gmail.com>
2019-03-28 17:51:41 +0300

    test

--- animations/AnimationExamples/App.js
+++ animations/AnimationExamples/App.js
@@ -3,11 +3,12 @@ import React, {Component} from 'react';
 import Timing from './components/Timing';
 import TranslatePosition from './components/TranslatePosition';
 import Scale from './components/Scale';
+import MultiField from './components/MultiField';
 
 export default class App extends Component {
 	render() {
 		return (
-			<Scale />
+			<MultiField />
 		);
 	}
 }
