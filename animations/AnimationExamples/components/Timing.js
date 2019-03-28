28695fe81bd1f0de92513bad092cb8adb9b053ea
Mehmet Seven <mehmetseven0@gmail.com>
2019-03-28 16:16:51 +0300

    animations timing

--- animations/AnimationExamples/components/Timing.js
+++ animations/AnimationExamples/components/Timing.js
@@ -1,14 +1,42 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Animated, TouchableWithoutFeedback } from 'react-native';
 
 export default class Timing extends Component {
-  render() {
-    return (
+	state = {
+		animation: new Animated.Value(1)
+	};
+
+	startAnimation = () => {
+		Animated
+			.timing(this.state.animation, {
+				toValue: 0,
+				duration: 500
+			})
+			.start()
+	};
+
+	render() {
+
+		this.state.animation.addListener(({value}) => console.log(value));
+
+		const animatedStyles = {
+			opacity: this.state.animation
+		};
+
+		return (
 			<View style={styles.container}>
-				<Text>React Native</Text>
+				<TouchableWithoutFeedback
+					onPress={this.startAnimation}
+				>
+					<Animated.View
+						style={[styles.myBox, animatedStyles ]}
+					>
+						<Text>React Native</Text>
+					</Animated.View>
+				</TouchableWithoutFeedback>
 			</View>
-    );
-  }
+		);
+	}
 }
 
 const styles = StyleSheet.create({
@@ -17,5 +45,12 @@ const styles = StyleSheet.create({
 		justifyContent: 'center',
 		alignItems: 'center',
 		backgroundColor: '#F5FCFF',
+	},
+	myBox: {
+		width: 200,
+		height: 200,
+		backgroundColor: '#FFC107',
+		justifyContent:'center',
+		alignItems:'center'
 	}
 });
