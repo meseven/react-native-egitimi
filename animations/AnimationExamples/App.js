61cbcde70f31ee68ff36cfb734332cfebb70196d
Mehmet Seven <mehmetseven0@gmail.com>
2019-03-28 16:40:57 +0300

    Transform

--- animations/AnimationExamples/App.js
+++ animations/AnimationExamples/App.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
+
 import Timing from './components/Timing';
+import TranslatePosition from './components/TranslatePosition';
 
 export default class App extends Component {
 	render() {
 		return (
-			<Timing />
+			<TranslatePosition />
 		);
 	}
 }
