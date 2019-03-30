f0ad136172624746eebe149b7bbc67bbdb7bc664
Mehmet Seven <mehmetseven0@gmail.com>
2019-03-31 02:38:54 +0300

    react-native-maps installation

--- maps/MapExercises/App.js
+++ maps/MapExercises/App.js
@@ -1,11 +1,21 @@
 import React, {Component} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 
+import MapView from 'react-native-maps';
+
 export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
+				<MapView
+          style={styles.map}
+					initialRegion={{
+						latitude: 37.78825,
+						longitude: -122.4324,
+						latitudeDelta: 0.0922,
+						longitudeDelta: 0.0421,
+					}}
+				/>
       </View>
     );
   }
@@ -14,8 +24,9 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
+  map: {
+    flex: 1
+  }
 });
