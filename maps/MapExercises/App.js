852cff1b82d5fc56e1d06c246b9447f8c3af68ee
Mehmet Seven <mehmetseven0@gmail.com>
2019-04-01 16:13:58 +0300

    region

--- maps/MapExercises/App.js
+++ maps/MapExercises/App.js
@@ -4,17 +4,34 @@ import {StyleSheet, Text, View} from 'react-native';
 import MapView from 'react-native-maps';
 
 export default class App extends Component {
-  render() {
+	state = {
+		region: {
+			latitude: 37.78825,
+			longitude: -122.4324,
+			latitudeDelta: 0.0922,
+			longitudeDelta: 0.0421,
+		}
+	};
+
+	componentDidMount() {
+		setTimeout(() => {
+			this.setState({
+				region: {
+					...this.state.region,
+					latitude: 38
+				},
+			});
+
+		}, 2000)
+	}
+
+
+	render() {
     return (
       <View style={styles.container}>
 				<MapView
           style={styles.map}
-					initialRegion={{
-						latitude: 37.78825,
-						longitude: -122.4324,
-						latitudeDelta: 0.0922,
-						longitudeDelta: 0.0421,
-					}}
+					region={this.state.region}
 				/>
       </View>
     );
