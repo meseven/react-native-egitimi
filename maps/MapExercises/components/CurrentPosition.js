ca637ba162ad32428ba0457a00f7a4781cf7523a
Mehmet Seven <mehmetseven0@gmail.com>
2019-04-01 22:30:53 +0300

    location permissions & simulator

--- maps/MapExercises/components/CurrentPosition.js
+++ maps/MapExercises/components/CurrentPosition.js
@@ -2,20 +2,31 @@ import React, { Component } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import MapView from "react-native-maps";
 
+import Permissions from 'react-native-permissions';
+
 export default class CurrentPosition extends Component {
 	state = {
 		region: {
-			latitude: 37.78825,
-			longitude: -122.4324,
+			latitude: 41.0087,
+			longitude: 29.0173,
 			latitudeDelta: 0.0922,
 			longitudeDelta: 0.0421,
 		},
 	};
 
+	componentDidMount() {
+		Permissions.request('location').then(response => {
+
+		})
+	}
+
+
 	render() {
 		return (
 			<View style={styles.container}>
 				<MapView
+					loadingEnabled={true}
+					showsUserLocation={true}
 					style={styles.map}
 					region={this.state.region}
 				>
