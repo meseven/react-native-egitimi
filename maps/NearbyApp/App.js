6a5fdeb37583d718234bba824073d51c9f1c19bb
Mehmet Seven <mehmetseven0@gmail.com>
2019-04-02 22:03:03 +0300

    map component

--- maps/NearbyApp/App.js
+++ maps/NearbyApp/App.js
@@ -1,60 +1,11 @@
 import React, {Component} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
 
-import MapView from 'react-native-maps';
+import Map from './components/Map';
 
 export default class App extends Component {
-	state = {
-		region: {
-			latitude: 41.0087,
-			longitude: 29.0173,
-			latitudeDelta: 0.0922,
-			longitudeDelta: 0.0421,
-		}
-	};
-
-	async componentDidMount() {
-		try{
-			const data = await this.getCurrentPosition();
-			console.log(data);
-		}catch (e) {
-			alert('Konum alınamadı!')
-		}
-	}
-
-	getCurrentPosition(){
-		return new Promise((resolve, reject) => {
-			navigator.geolocation.getCurrentPosition(
-				position => resolve(position), // success
-				() => reject(), // fail
-				{
-					timeout: 5000,
-					maximumAge: 1000,
-					enableHighAccuracy: false
-				}
-			)
-		})
-	}
-
 	render() {
     return (
-      <View style={styles.container}>
-				<MapView
-          style={styles.map}
-					showsUserLocation={true}
-					region={this.state.region}
-				/>
-      </View>
+			<Map />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#F5FCFF',
-  },
-  map: {
-    flex: 1
-  }
-});
