bcff14dfaba617e6e09b5e19eaf5ac827c47d564
Mehmet Seven <mehmetseven0@gmail.com>
2019-04-02 21:43:47 +0300

    getCurrentPosition

--- maps/NearbyApp/App.js
+++ maps/NearbyApp/App.js
@@ -13,6 +13,29 @@ export default class App extends Component {
 		}
 	};
 
+	async componentDidMount() {
+		try{
+			const data = await this.getCurrentPosition();
+			console.log(data);
+		}catch (e) {
+			alert('Konum alınamadı!')
+		}
+	}
+
+	getCurrentPosition(){
+		return new Promise((resolve, reject) => {
+			navigator.geolocation.getCurrentPosition(
+				position => resolve(position), // success
+				() => reject(), // fail
+				{
+					timeout: 5000,
+					maximumAge: 1000,
+					enableHighAccuracy: false
+				}
+			)
+		})
+	}
+
 	render() {
     return (
       <View style={styles.container}>
