bc5d614121800c6af6d13bc3492d4ff57847836b
Mehmet Seven <mehmetseven0@gmail.com>
2019-04-04 21:13:43 +0300

    test

--- forms/FormExamples/App.js
+++ forms/FormExamples/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 
 export default class App extends Component {
   render() {
