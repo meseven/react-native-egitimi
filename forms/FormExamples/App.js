66b95b571e75087033870ef180619dec60a364ed
Mehmet Seven <mehmetseven0@gmail.com>
2019-04-04 17:05:25 +0300

    forms

---
+++ forms/FormExamples/App.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  }
+});
