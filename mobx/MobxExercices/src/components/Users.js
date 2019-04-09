855eb360376af67c260d11a094d4aeab3671177c
Mehmet Seven <mehmetseven0@gmail.com>
2019-04-09 23:40:10 +0300

    activity indicator

--- mobx/MobxExercices/src/components/Users.js
+++ mobx/MobxExercices/src/components/Users.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, ActivityIndicator } from 'react-native';
 
 import {observer,inject} from 'mobx-react';
 
@@ -10,10 +10,9 @@ export default class Users extends Component {
   	const {UserStore} = this.props;
     return (
       <View>
-				<Button
-					title={"fetch users"}
-					onPress={() => UserStore.fetchUsers()}
-				/>
+				{
+					UserStore.loading ? <ActivityIndicator size={"small"} /> : <Button title={"fetch users"} onPress={() => UserStore.fetchUsers()}/>
+				}
 
 				<View>
 					{
--- mobx/MobxExercices/src/store/UserStore.js
+++ mobx/MobxExercices/src/store/UserStore.js
@@ -7,8 +7,11 @@ configure({
 
 class UserStore{
 	@observable users = [];
+	@observable loading = false;
 
 	@action fetchUsers (){
+		this.users = [];
+		this.loading = true;
 		axios
 			.get('https://randomuser.me/api/?results=10')
 			.then(response => response.data.results)
@@ -16,10 +19,12 @@ class UserStore{
 	}
 
 	@action.bound fetchUsersSuccess(users){
+		this.loading = false;
 		this.users = users;
 	}
 
 	@action.bound fetchUsersError(){
+		this.loading = false;
 		alert('error')
 	}
 }
