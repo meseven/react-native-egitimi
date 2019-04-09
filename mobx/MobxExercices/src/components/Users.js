9406aca05519d0d5f0bd640c8fc6a31945793b79
Mehmet Seven <mehmetseven0@gmail.com>
2019-04-09 22:57:15 +0300

    fetchUsers

--- mobx/MobxExercices/src/components/Users.js
+++ mobx/MobxExercices/src/components/Users.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
+import {observer,inject} from 'mobx-react';
+
+@inject("UserStore")
+@observer
 export default class Users extends Component {
   render() {
+  	const {UserStore} = this.props;
     return (
       <View>
-				<Text>users</Text>
+				<Button
+					title={"fetch users"}
+					onPress={() => UserStore.fetchUsers()}
+				/>
+
+				<View>
+					{
+						UserStore.users.map((user, key) => (
+							<View key={key}>
+								<Text>{ user.name.first }</Text>
+							</View>
+						))
+					}
+				</View>
 			</View>
     );
   }
--- mobx/MobxExercices/src/store/UserStore.js
+++ mobx/MobxExercices/src/store/UserStore.js
@@ -1,11 +1,27 @@
-import {observable,configure} from 'mobx';
+import {observable,configure, action} from 'mobx';
+import axios from 'axios';
 
 configure({
 	enforceActions: "observed"
 });
 
 class UserStore{
+	@observable users = [];
 
+	@action fetchUsers (){
+		axios
+			.get('https://randomuser.me/api/?results=10')
+			.then(response => response.data.results)
+			.then(this.fetchUsersSucces, this.fetchUsersError)
+	}
+
+	@action fetchUsersSucces = (users) => {
+		this.users = users;
+	}
+
+	@action fetchUsersError = () => {
+		alert('error')
+	}
 }
 
 export default new UserStore()
