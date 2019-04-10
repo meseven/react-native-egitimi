d58e5aeb1eb751bcf47deda6abf02a75d459f7d5
Mehmet Seven <mehmetseven0@gmail.com>
2019-04-11 00:43:35 +0300

    auth forms

--- mobx/MovieApp/src/screens/SignIn/index.js
+++ mobx/MovieApp/src/screens/SignIn/index.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { StyleSheet} from 'react-native';
+
+import { Container, Header, Content, Form, Item, Input, Body, Title, Button, Text } from 'native-base';
 
 export default class SignIn extends Component {
   render() {
     return (
-			<SafeAreaView>
-				<View>
-					<Text>signin</Text>
-				</View>
-			</SafeAreaView>
+			<Container>
+				<Header>
+					<Body>
+						<Title>Sign In</Title>
+					</Body>
+				</Header>
+				<Content>
+					<Form>
+						<Item>
+							<Input placeholder="Username" />
+						</Item>
+						<Item last>
+							<Input
+								secureTextEntry={true}
+								placeholder="Password" />
+						</Item>
+					</Form>
+					<Button full style={{ margin: 10 }}>
+						<Text>Sign In</Text>
+					</Button>
+				</Content>
+			</Container>
     );
   }
 }
--- mobx/MovieApp/src/screens/SignUp/index.js
+++ mobx/MovieApp/src/screens/SignUp/index.js
@@ -1,14 +1,37 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { StyleSheet } from 'react-native';
+import {Body, Button, Container, Content, Form, Header, Input, Item, Title, Text} from "native-base";
 
 export default class SignUp extends Component {
 	render() {
 		return (
-			<SafeAreaView>
-				<View>
-					<Text>SignUp</Text>
-				</View>
-			</SafeAreaView>
+			<Container>
+				<Header>
+					<Body>
+						<Title>Sign Up</Title>
+					</Body>
+				</Header>
+				<Content>
+					<Form>
+						<Item>
+							<Input placeholder="Username" />
+						</Item>
+						<Item last>
+							<Input
+								secureTextEntry={true}
+								placeholder="Password" />
+						</Item>
+						<Item last>
+							<Input
+								secureTextEntry={true}
+								placeholder="Password confirm" />
+						</Item>
+					</Form>
+					<Button full style={{ margin: 10 }}>
+						<Text>Sign Up</Text>
+					</Button>
+				</Content>
+			</Container>
 		);
 	}
 }
