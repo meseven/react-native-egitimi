8282fa6f1b9583f027b16bc1199f74036bc79642
Mehmet Seven <mehmetseven0@gmail.com>
2019-04-11 19:19:52 +0300

    revert to a7dd323

--- mobx/MovieApp/src/screens/SignIn/index.js
+++ mobx/MovieApp/src/screens/SignIn/index.js
@@ -1,25 +1,15 @@
 import React, { Component } from 'react';
-import { StyleSheet} from 'react-native';
-
-import { Container, Header, Content, Body, Title } from 'native-base';
-
-// signin form
-import SignInForm from './SignInForm';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 export default class SignIn extends Component {
   render() {
-		return (
-			<Container>
-				<Header>
-					<Body>
-						<Title>Sign In</Title>
-					</Body>
-				</Header>
-				<Content style={{ padding: 10 }}>
-					<SignInForm />
-				</Content>
-			</Container>
-		);
+    return (
+			<SafeAreaView>
+				<View>
+					<Text>signin</Text>
+				</View>
+			</SafeAreaView>
+    );
   }
 }
 
--- mobx/MovieApp/src/screens/SignUp/index.js
+++ mobx/MovieApp/src/screens/SignUp/index.js
@@ -1,22 +1,14 @@
 import React, { Component } from 'react';
-import { StyleSheet } from 'react-native';
-import {Body, Button, Container, Content, Header, Input, Item, Title, Text} from "native-base";
-
-import SignUpForm from './SignUpForm';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 export default class SignUp extends Component {
 	render() {
 		return (
-			<Container>
-				<Header>
-					<Body>
-						<Title>Sign Up</Title>
-					</Body>
-				</Header>
-				<Content style={{ padding: 10 }}>
-					<SignUpForm />
-				</Content>
-			</Container>
+			<SafeAreaView>
+				<View>
+					<Text>SignUp</Text>
+				</View>
+			</SafeAreaView>
 		);
 	}
 }
