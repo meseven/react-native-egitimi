aeb3643292f978a9acd4224e909d47def210c878
Mehmet Seven <mehmetseven0@gmail.com>
2019-04-13 21:35:25 +0300

    online count

--- real-time/SocketIOExample/backend/server.js
+++ real-time/SocketIOExample/backend/server.js
@@ -3,10 +3,18 @@ const io = require('socket.io')(http);
 
 http.listen(3000);
 
+let count = 0;
+
 io.on('connection', socket => {
 	console.log('new user');
 
+	count++;
+
+	io.emit('newUser', count);
+
 	socket.on('disconnect', () => {
+		count--;
+		io.emit('disUser', count);
 		console.log('user disconnected');
 	})
 });
--- real-time/SocketIOExample/mobile/App.js
+++ real-time/SocketIOExample/mobile/App.js
@@ -4,16 +4,35 @@ import {StyleSheet, Text, View} from 'react-native';
 import io from 'socket.io-client';
 
 export default class App extends Component {
+  state = {
+    onlineCount: 0
+  };
+
   componentDidMount() {
     this.io = io.connect('http://localhost:3000', {
       timeout: 10000
+    });
+
+    this.io.on('newUser', onlineCount => {
+      this.setState({
+        onlineCount,
+      });
+    });
+
+    this.io.on('disUser', onlineCount => {
+      this.setState({
+        onlineCount,
+      });
     })
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <Text>Welcome to React Native!</Text>
+        <View style={styles.countContainer}>
+          <Text style={styles.count}>{this.state.onlineCount}</Text>
+          <Text>Online now</Text>
+        </View>
       </View>
     );
   }
@@ -24,5 +43,11 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center'
+  },
+  countContainer: {
+    alignItems: 'center'
+  },
+  count: {
+    fontSize: 32
   }
 });
