a76ebd7e8cf8cd74eaf126f9332d185e58ea56c2
Mehmet Seven <mehmetseven0@gmail.com>
2019-04-15 19:29:34 +0300

    socketio

--- real-time/SocketIOExample/mobile/android/app/src/main/java/com/socketioexample/MainApplication.java
+++ real-time/SocketIOExample/mobile/android/app/src/main/java/com/socketioexample/MainApplication.java
@@ -3,6 +3,7 @@ package com.socketioexample;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -22,7 +23,8 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new ReactNativePushNotificationPackage()
       );
     }
 
