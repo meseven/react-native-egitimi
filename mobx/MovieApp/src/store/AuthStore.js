e8ce8296486456fc28715c148b8155e98b0a25ee
Mehmet Seven <mehmetseven0@gmail.com>
2019-04-17 01:06:35 +0300

    auth store save token

--- mobx/MovieApp/src/store/AuthStore.js
+++ mobx/MovieApp/src/store/AuthStore.js
@@ -1,7 +1,16 @@
-import {observable} from 'mobx';
+import {observable, action} from 'mobx';
+import AsyncStorage from '@react-native-community/async-storage';
 
 class AuthStore{
-	@observable username = "mseven";
+	@observable token = null;
+
+	@action async saveToken(token){
+		try{
+			await AsyncStorage.setItem('token', token);
+		}catch (e) {
+			console.log(e);
+		}
+	}
 }
 
 export default new AuthStore();
