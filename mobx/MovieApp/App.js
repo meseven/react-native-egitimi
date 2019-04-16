0ae9cff52f76ce65a5ddb875493c71f14450791f
Mehmet Seven <mehmetseven0@gmail.com>
2019-04-17 01:35:33 +0300

    navigation service

--- mobx/MovieApp/App.js
+++ mobx/MovieApp/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 
 import Router from './src/Router';
+import NavigationService from './src/NavigationService';
 
 // mobx store
 import store from './src/store';
@@ -10,7 +11,11 @@ export default class App extends Component {
   render() {
     return (
       <Provider {...store}>
-        <Router/>
+        <Router
+          ref={navigatorRef => {
+            NavigationService.setTopLevelNavigator(navigatorRef);
+          }}
+        />
       </Provider>
     );
   }
--- mobx/MovieApp/src/store/AuthStore.js
+++ mobx/MovieApp/src/store/AuthStore.js
@@ -1,12 +1,16 @@
 import {observable, action} from 'mobx';
 import AsyncStorage from '@react-native-community/async-storage';
 
+// navigation service
+import NavigationService from '../NavigationService';
+
 class AuthStore{
 	@observable token = null;
 
 	@action async saveToken(token){
 		try{
 			await AsyncStorage.setItem('token', token);
+			console.log(NavigationService);
 		}catch (e) {
 			console.log(e);
 		}
