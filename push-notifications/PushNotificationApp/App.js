13e40f85d44e22cdd651587d313740ac02e324a5
Mehmet Seven <mehmetseven0@gmail.com>
2019-05-26 02:20:27 +0300

    notify

--- push-notifications/PushNotificationApp/App.js
+++ push-notifications/PushNotificationApp/App.js
@@ -11,6 +11,47 @@ export default class App extends Component{
   }
 }
 
+var PushNotification = require('react-native-push-notification');
+
+PushNotification.configure({
+
+  // (optional) Called when Token is generated (iOS and Android)
+  onRegister: function(token) {
+    console.log( 'TOKEN:', token );
+  },
+
+  // (required) Called when a remote or local notification is opened or received
+  onNotification: function(notification) {
+    console.log( 'NOTIFICATION:', notification );
+
+    // process the notification
+
+    // required on iOS only (see fetchCompletionHandler docs: https://facebook.github.io/react-native/docs/pushnotificationios.html)
+    notification.finish(PushNotificationIOS.FetchResult.NoData);
+  },
+
+  // ANDROID ONLY: GCM or FCM Sender ID (product_number) (optional - not required for local notifications, but is need to receive remote push notifications)
+  senderID: "YOUR GCM (OR FCM) SENDER ID",
+
+  // IOS ONLY (optional): default: all - Permissions to register.
+  permissions: {
+    alert: true,
+    badge: true,
+    sound: true
+  },
+
+  // Should the initial notification be popped automatically
+  // default: true
+  popInitialNotification: true,
+
+  /**
+   * (optional) default: true
+   * - Specified if permissions (ios) and token (android and ios) will requested or not,
+   * - if not, you must call PushNotificationsHandler.requestPermissions() later
+   */
+  requestPermissions: true,
+});
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
