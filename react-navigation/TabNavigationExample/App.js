670af6e373b2a266dea95454d42d3ac622bb7b92
Mehmet Seven <mehmetseven0@gmail.com>
2020-02-22 00:44:30 +0300

    react-navigation bug fix

--- react-navigation/TabNavigationExample/App.js
+++ react-navigation/TabNavigationExample/App.js
@@ -1,10 +1,8 @@
-import React, {Component, Fragment} from 'react';
-import Router from './src/Router';
+import React, { Component, Fragment } from "react";
+import Router from "./src/Router";
 
 export default class App extends Component {
 	render() {
-		return (
-			<Router/>
-		);
+		return <Router />;
 	}
 }
--- react-navigation/TabNavigationExample/src/Router.js
+++ react-navigation/TabNavigationExample/src/Router.js
@@ -1,30 +1,30 @@
-import React from 'react';
+import React from "react";
 
 import {
 	createStackNavigator,
 	createBottomTabNavigator,
 	createAppContainer,
 	createDrawerNavigator
-} from 'react-navigation';
+} from "react-navigation";
 
-import Icon from 'react-native-vector-icons/Ionicons';
+import Icon from "react-native-vector-icons/Ionicons";
 
 // screens
-import Home from './screens/Home';
+import Home from "./screens/Home";
 
-import Contacts from './screens/Contacts';
-import ContactDetail from './screens/ContactDetail';
+import Contacts from "./screens/Contacts";
+import ContactDetail from "./screens/ContactDetail";
 
-import Settings from './screens/Settings';
-import SettingsModal from './components/SettingsModal';
+import Settings from "./screens/Settings";
+import SettingsModal from "./components/SettingsModal";
 
-import DrawerMenu from './components/DrawerMenu';
+import DrawerMenu from "./components/DrawerMenu";
 
 const ContactStack = createStackNavigator({
 	Contacts: {
 		screen: Contacts,
 		navigationOptions: {
-			title: 'Contacts'
+			title: "Contacts"
 		}
 	},
 	ContactDetail: {
@@ -32,55 +32,70 @@ const ContactStack = createStackNavigator({
 	}
 });
 
-const TabNavigator = createBottomTabNavigator({
-	Home: {
-		screen: Home,
-		navigationOptions: {
-			tabBarIcon: ({ tintColor }) => (<Icon name="ios-home" size={22} color={tintColor} />)
+const TabNavigator = createBottomTabNavigator(
+	{
+		Home: {
+			screen: Home,
+			navigationOptions: {
+				tabBarIcon: ({ tintColor }) => (
+					<Icon name="ios-home" size={22} color={tintColor} />
+				)
+			}
+		},
+		Contacts: {
+			screen: ContactStack,
+			navigationOptions: {
+				tabBarIcon: ({ tintColor }) => (
+					<Icon name="ios-contacts" size={22} color={tintColor} />
+				)
+			}
+		},
+		Settings: {
+			screen: Settings,
+			navigationOptions: {
+				tabBarIcon: ({ tintColor }) => (
+					<Icon name="ios-settings" size={22} color={tintColor} />
+				)
+			}
 		}
 	},
-	Contacts: {
-		screen: ContactStack,
-		navigationOptions: {
-			tabBarIcon: ({ tintColor }) => (<Icon name="ios-contacts" size={22} color={tintColor} />)
-		}
-	},
-	Settings: {
-		screen: Settings,
-		navigationOptions: {
-			tabBarIcon: ({ tintColor }) => (<Icon name="ios-settings" size={22} color={tintColor} />)
-		}
+	{
+		tabBarOptions: {
+			showLabel: false,
+			activeTintColor: "#f8f8f8",
+			inactiveTintColor: "#586589",
+			style: {
+				backgroundColor: "#171f33"
+			}
+		},
+		initialRouteName: "Contacts"
 	}
-}, {
-	tabBarOptions: {
-		showLabel: false,
-		activeTintColor: '#f8f8f8',
-		inactiveTintColor: '#586589',
-		style: {
-			backgroundColor: '#171f33'
+);
+
+const ModalStack = createStackNavigator(
+	{
+		Tabs: {
+			screen: TabNavigator
+		},
+		SettingsModal: {
+			screen: SettingsModal
 		}
 	},
-	initialRouteName: 'Contacts'
-});
-
-const ModalStack = createStackNavigator({
-	Tabs: {
-		screen: TabNavigator
-	},
-	SettingsModal: {
-		screen: SettingsModal
+	{
+		mode: "modal",
+		headerMode: "none"
 	}
-},{
-	mode: 'modal',
-	headerMode: 'none'
-});
+);
 
-const DrawerNavigator = createDrawerNavigator({
-	ModalStack: {
-		screen: ModalStack
+const DrawerNavigator = createDrawerNavigator(
+	{
+		ModalStack: {
+			screen: ModalStack
+		}
 	},
-}, {
-	contentComponent: DrawerMenu
-});
+	{
+		// contentComponent: DrawerMenu
+	}
+);
 
 export default createAppContainer(DrawerNavigator);
