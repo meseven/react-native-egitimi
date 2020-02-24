a6e7b582119a7acd1890105f4e6c970924fee37a
Mehmet Seven <mehmetseven0@gmail.com>
2020-02-24 14:25:29 +0300

    Pokemons graphql query.

--- pokeapp/src/screens/Pokemons/Pokemons.js
+++ pokeapp/src/screens/Pokemons/Pokemons.js
@@ -1,7 +1,31 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 
+import {useQuery} from '@apollo/react-hooks';
+import {gql} from 'apollo-boost';
+
+const POKEMONS_QUERY = gql`
+	{
+		pokemons(first: 10) {
+			id
+			name
+		}
+	}
+`;
+
 const Pokemons = () => {
+	const {error, loading, data} = useQuery(POKEMONS_QUERY);
+
+	if (loading) {
+		return <Text>Loading...</Text>;
+	}
+
+	if (error) {
+		return <Text>Error.</Text>;
+	}
+
+	console.log(data);
+
 	return (
 		<View>
 			<Text>Pokemons</Text>
