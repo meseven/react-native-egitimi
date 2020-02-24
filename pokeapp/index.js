d9e6aa614d4af72a06057caa13e9e022f76cf7ce
Mehmet Seven <mehmetseven0@gmail.com>
2020-02-24 14:07:31 +0300

    Apollo client installation.

--- pokeapp/index.js
+++ pokeapp/index.js
@@ -1,6 +1,23 @@
+import React from 'react';
 import {AppRegistry} from 'react-native';
-// import App from './App';
+
+import {ApolloProvider} from '@apollo/react-hooks';
+import {ApolloClient, HttpLink, InMemoryCache} from 'apollo-boost';
+
 import Router from './src/Router';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => Router);
+const client = new ApolloClient({
+	link: new HttpLink({
+		uri: 'https://graphql-pokemon.now.sh/',
+	}),
+	cache: new InMemoryCache(),
+});
+
+const App = () => (
+	<ApolloProvider client={client}>
+		<Router />
+	</ApolloProvider>
+);
+
+AppRegistry.registerComponent(appName, () => App);
